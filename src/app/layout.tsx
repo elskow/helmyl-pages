1aca65fd611433bcfa6a55b7fc7037cfe86d6912
elskow <elskow@outlook.com>
2023-12-23 23:04:53 +0700

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import "@/styles/globals.css";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Helmyl Pages",
+  description: "A personal website of Helmy",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body
+        className={
+          font.className + " selection:bg-teal-400  bg-zinc-50 text-black"
+        }
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <main className="flex flex-col items-center justify-center min-h-screen py-2 pb-10">
+        <h1 className="text-lg font-semibold md:text-xl px-5 py-2">
+          Under construction !
+          <span className="px-3 animate-pulse cursor-not-allowed">🚧</span>
+        </h1>
+      </main>
+    </>
+  );
+}
