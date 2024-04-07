462700ab02b24d862b0919ad561c9f60d01811dc
elskow <elskow@outlook.com>
2024-04-08 00:28:27 +0700

    feat: add auto update to the spotify components (interval approach), add button and sidebar image banner

--- src/components/SpotifyNowPlayin.tsx
+++ src/components/SpotifyNowPlayin.tsx
@@ -3,6 +3,8 @@
 import { useEffect, useState } from 'react'
 import { FaSpotify } from 'react-icons/fa'
 import { AiOutlineLoading } from 'react-icons/ai'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const SpotifyNowPlayin = () => {
     const [nowPlaying, setNowPlaying] = useState<any>({
@@ -31,6 +33,12 @@ const SpotifyNowPlayin = () => {
         }
 
         fetchNowPlaying().then((r) => r)
+
+        const interval = setInterval(() => {
+            fetchNowPlaying().then((r) => r)
+        }, 5000) // 5 secs
+
+        return () => clearInterval(interval)
     }, [])
 
     if (loading) {
@@ -52,20 +60,32 @@ const SpotifyNowPlayin = () => {
                 <h1 className="text-sm font-bold lg:text-lg">Now Playing</h1>
             </div>
             <div
-                className="flex flex-row justify-items-start gap-6 rounded-lg bg-gray-100 drop-shadow-md dark:bg-gray-800"
+                className="flex flex-row justify-between justify-items-start gap-6 rounded-lg bg-gray-100 drop-shadow-md dark:bg-gray-800"
                 style={{
                     backgroundImage: `url(${nowPlaying.image})`,
                     backgroundSize: 'cover',
-                    opacity: 1,
                 }}
             >
-                <div className="flex h-full w-full flex-col justify-center justify-items-center rounded-lg bg-green-900 bg-opacity-60 px-4 py-4 md:px-6 md:py-6">
-                    <h1 className="mb-1 line-clamp-1 items-center justify-self-center pt-2 text-sm font-bold text-gray-200 lg:text-lg">
-                        {nowPlaying.title}
-                    </h1>
-                    <h2 className="mb-2 line-clamp-1 items-center justify-center text-sm font-medium text-gray-300 lg:text-base">
-                        {nowPlaying.artist}
-                    </h2>
+                <div className="flex h-full w-full flex-row items-center justify-between rounded-lg bg-green-900 bg-opacity-60 px-4 py-4 md:px-6 md:py-6">
+                    <div>
+                        <h1 className="mb-1 line-clamp-1 items-center justify-self-center pt-2 text-sm font-bold text-gray-200 lg:text-lg">
+                            {nowPlaying.title}
+                        </h1>
+                        <h2 className="mb-2 line-clamp-1 items-center justify-center text-sm font-medium text-gray-300 lg:text-base">
+                            {nowPlaying.artist}
+                        </h2>
+                    </div>
+                    <div>
+                        <Link href={nowPlaying.url} passHref>
+                            <Image
+                                src={nowPlaying.image}
+                                alt="Now Playing"
+                                width={100}
+                                height={100}
+                                className={`rounded-lg shadow-lg filter transition-all duration-300 hover:shadow-xl hover:brightness-90`}
+                            />
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
