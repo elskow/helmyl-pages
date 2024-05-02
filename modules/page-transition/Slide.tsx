cf67092523094dbe09c949c493362ac8f36fc6fe
elskow <elskow@outlook.com>
2024-05-02 09:51:00 +0700

    ui: remove on view animation for consistent ui

--- modules/page-transition/Slide.tsx
+++ modules/page-transition/Slide.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { motion, useAnimation, useInView } from 'framer-motion'
+
+import { motion, useAnimation } from 'framer-motion'
 import React, { useEffect, useRef } from 'react'
 
 type SlideProps = {
@@ -10,25 +11,30 @@ type SlideProps = {
 
 export const Slide = ({ children, className, delay = 0 }: SlideProps) => {
     const ref = useRef(null)
-    const isInview = useInView(ref, { once: true })
     const controls = useAnimation()
 
     useEffect(() => {
-        if (isInview) {
-            controls.start('stop')
-        }
-    }, [controls, isInview])
+        controls.start('stop')
+    }, [controls])
 
     return (
         <motion.div
             ref={ref}
             variants={{
-                start: { opacity: 0, translateY: 10 },
-                stop: { opacity: 1, translateY: 0 },
+                start: {
+                    opacity: 0,
+                    translateY: 10,
+                    pointerEvents: 'none',
+                },
+                stop: {
+                    opacity: 1,
+                    translateY: 0,
+                    pointerEvents: 'auto',
+                },
             }}
             transition={{
                 ease: 'easeInOut',
-                duration: 0.3,
+                duration: 0.2,
                 delay: delay,
                 stiffness: 0.5,
             }}
